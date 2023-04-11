67259514f0562fd066f19b48eb69a42ccff3ef3c
Stanislav Miednikov <luxshd@gmail.com>
2023-04-11 22:49:25 +0300

    Homework resending

---
+++ Lesson 2/homework 4/homework4.js
@@ -0,0 +1,4 @@
+let userTime = Number(prompt('Введите количество часов:'));
+userTime *= 3600;
+
+alert(`Это ${userTime} секунд`);
---
+++ Lesson 2/homework 5/homework5.js
@@ -0,0 +1,5 @@
+const userNum1 = Number(prompt('Введите первое число:')),
+    userNum2 = Number(prompt('Введите второе число:')),
+    userNum3 = Number(prompt('Введите третье число:'));
+
+alert(`Среднее арифметическое чисел ${userNum1}, ${userNum2} и ${userNum3} составляет ${(userNum1 + userNum2 + userNum3) / 3}`);
