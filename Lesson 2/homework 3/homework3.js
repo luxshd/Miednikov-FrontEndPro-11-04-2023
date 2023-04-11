5d3f3cc5462c6a318309a5997686d6d9fb207ff6
Stanislav Miednikov <luxshd@gmail.com>
2023-04-11 22:46:06 +0300

    Added homeworks 3,4 and 5

---
+++ Lesson 2/homework 3/homework3.js
@@ -0,0 +1,8 @@
+const userNum1 = Number(prompt('Введите первое число:'));
+const userNum2 = Number(prompt('Введите второе число:'));
+
+alert(`
+       ${userNum1} + ${userNum2} = ${userNum1 + userNum2}\n
+       ${userNum1} - ${userNum2} = ${userNum1 - userNum2}\n
+       ${userNum1} * ${userNum2} = ${userNum1 * userNum2}\n
+       ${userNum1} / ${userNum2} = ${userNum1 / userNum2}`)
