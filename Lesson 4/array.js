ca984cbe3c05ad4fffbbc03678a0ee6bd30ca856
python_basic <luxshd@gmail.com>
2023-04-18 10:42:26 +0300

    Add homework 8 (array)

--- Lesson 4/array.js
+++ Lesson 4/array.js
@@ -9,7 +9,7 @@ console.log('Empty array', userArray);
 let userElements = '';
 
 // Понимаю, что while ещё не проходили, но хотел заставить пользователя ввести кол-во элементов равное заданной длине
-while (userElements.split(' ').length != USER_ARRAY_LENGTH){
+while (userElements.split(' ').length !== USER_ARRAY_LENGTH){
     userElements = prompt(`Введите элементы массива через пробел в количестве ${USER_ARRAY_LENGTH} шт.`);
 }
 userArray = userElements.split(' ');
@@ -23,7 +23,7 @@ console.log('Sorted array', userArray);
 // Удаляю элементы массива, при условии, что длина массива это позволяет
 if (userArray.length >= 4){
     userArray.splice(1, 3);
+    console.log('Spliced array', userArray);
 } else {
     console.log(`Срез массива со 2-го по 4-й элементы включительно невозможен, т.к. массив имеет длину ${userArray.length}`);
 }
-console.log('Spliced array', userArray);
