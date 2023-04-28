b49b5caeded406e10ad7ce90971d43ef7ac83833
python_basic <luxshd@gmail.com>
2023-04-28 11:59:40 +0300

    Add homework 12 (generate password)

--- Lesson 6/Generate key/homework12.js
+++ Lesson 6/Generate key/homework12.js
@@ -4,29 +4,13 @@
 // Функция для получения рандомного индекса массива
 const getRandomArrayIndex = (arr) => Math.floor(Math.random() * arr.length);
 
-// Функция для установки рандомного регистра символу, если он не числовой
-const setRandomRegister = (char) => {
-    if (!!Number(char)) {
-        return char
-    }
-    const random = Math.floor(Math.random() * 10);
-    if (random < 5) {
-        return char.toUpperCase();
-    }
-    return char.toLowerCase();
-}
-
 // Функция, генерирующая случайный пароль
 function generateKey(passLength, arr) {
     let result = '';
     for (let i = 0; i < passLength; i++) {
-        // Получаем случайный элемент массива символов
-        let char = arr[getRandomArrayIndex(arr)]
-
-        // Если элемент массива - буква, случайно выбираем регистр и сохраняем в результат
-        result += setRandomRegister(char)
+        // Получаем случайный элемент массива символов и сохраняем в строку результата
+        result+= arr[getRandomArrayIndex(arr)]
     }
-
     return result;
 }
 
