0377c7b6e16e3bc15ec2314d3d4fffdaf4a4e700
python_basic <luxshd@gmail.com>
2023-05-06 11:02:31 +0300

    Add homework 15 (This chain - Ladder)

---
+++ Lesson 8/This chain/homework17.js
@@ -0,0 +1,26 @@
+// Це ladder (сходи) – об'єкт, який дозволяє підніматися вгору та спускатися:
+
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this
+    },
+    down: function () {
+        this.step--;
+        return this
+    },
+    showStep: function () { // показывает текущую ступеньку
+        alert(this.step);
+        return this
+    }
+};
+// Тепер, якщо нам потрібно зробити кілька послідовних викликів, ми можемо виконати це так:
+ladder.up();
+ladder.up();
+ladder.down();
+ladder.showStep(); // 1
+
+// Змініть код методів up, down і showStep таким Таким чином, щоб їх виклик можна було зробити по ланцюжку, наприклад:
+//
+ladder.up().up().down().showStep(); // 1
