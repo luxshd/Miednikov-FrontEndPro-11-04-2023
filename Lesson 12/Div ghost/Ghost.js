c7c3b12ab6d4e2214af17ce857df598001e6ded3
luxsh <luxshd@gmail.com>
2023-05-19 08:39:59 +0300

    Add hw 12 (ghost div)

--- Lesson 12/Div ghost/Ghost.js
+++ Lesson 12/Div ghost/Ghost.js
@@ -1,5 +1,13 @@
+// Создаём элементы
 const body = document.querySelector('body')
+const input = document.createElement('input')
+const div = document.createElement('div')
 
+// Вставляем элементы в разметку
+document.body.append(input)
+document.body.append(div)
+
+// Стили
 // Body styles
 body.style.margin = '0'
 body.style.padding = '0'
@@ -11,11 +19,9 @@ body.style.display = 'flex'
 body.style.flexDirection = 'column'
 body.style.alignItems = 'center'
 
-const input = document.createElement('input')
 // Input styles
 input.style.fontSize='20px'
 
-const div = document.createElement('div')
 // Div styles
 div.style.width = '200px';
 div.style.height = '200px';
@@ -30,11 +36,10 @@ div.style.fontWeight = '800'
 div.style.fontSize = '30px'
 div.style.textAlign = 'center'
 div.style.display = 'none'
+
 div.innerText = 'YOU CAN SEE ME'
 
-document.body.append(input)
-document.body.append(div)
-
+// Само домашнее задание
 input.addEventListener('focusin', () => {
     div.style.display = 'flex'
 })
