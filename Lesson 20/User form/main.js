0e77f41286b3738e1359f94a42701bc17973d558
luxsh <luxshd@gmail.com>
2023-07-04 15:44:12 +0300

    Homework 31 Form

--- Lesson 20/User form/main.js
+++ Lesson 20/User form/main.js
@@ -1,3 +1,11 @@
-export function getFormInfo(){
+window.addEventListener('click', (e) =>{
+    if(e.target){
 
+    }
+})
+function getFormInfo(){
+    const formData = new FormData();
+    const [a,n,c] = formData;
+    console.log(a,n,c)
 }
+getFormInfo()
