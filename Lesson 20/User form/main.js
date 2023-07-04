d92d7db2103a302482b0ef17ca4a01bbbe72057c
luxsh <luxshd@gmail.com>
2023-07-04 17:32:44 +0300

    Homework 31 Form

--- Lesson 20/User form/main.js
+++ Lesson 20/User form/main.js
@@ -1,11 +1,20 @@
-window.addEventListener('click', (e) =>{
-    if(e.target){
-
-    }
-})
-function getFormInfo(){
-    const formData = new FormData();
-    const [a,n,c] = formData;
-    console.log(a,n,c)
-}
-getFormInfo()
+// window.addEventListener('click', (e) =>{
+//     if(e.target.type === 'submit'){
+//         const form = document.querySelector('form');
+//         const formData = new FormData(form);
+//         const data = formData.values();
+//         let [name, birthDate, sex, city, address, languages] = data;
+//         for(var pair of formData.entries()) {
+//             console.log(pair[0]+ ', '+ pair[1]);
+//         }
+//         const body = document.querySelector('body');
+//         form.style.display = 'none';
+//         body.innerHTML = `<p>Имя: ${name}</p>
+//                      <p>Дата рождения: ${birthDate}</p>
+//                      <p>Пол: ${sex}</p>
+//                      <p>Город: ${city}</p>
+//                      <p>Адрес: ${address}</p>
+//                      <p>Языки: ${languages}</p>`
+//     }
+//
+// })
