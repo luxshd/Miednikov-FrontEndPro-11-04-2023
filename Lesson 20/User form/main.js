d029282a2b1fafb8f0c1ef349b268bf3dd9267e4
luxsh <luxshd@gmail.com>
2023-07-04 17:34:45 +0300

    Homework 31 Form

--- Lesson 20/User form/main.js
+++ Lesson 20/User form/main.js
@@ -1,20 +1,31 @@
-// window.addEventListener('click', (e) =>{
-//     if(e.target.type === 'submit'){
-//         const form = document.querySelector('form');
-//         const formData = new FormData(form);
-//         const data = formData.values();
-//         let [name, birthDate, sex, city, address, languages] = data;
-//         for(var pair of formData.entries()) {
-//             console.log(pair[0]+ ', '+ pair[1]);
-//         }
-//         const body = document.querySelector('body');
-//         form.style.display = 'none';
-//         body.innerHTML = `<p>Имя: ${name}</p>
-//                      <p>Дата рождения: ${birthDate}</p>
-//                      <p>Пол: ${sex}</p>
-//                      <p>Город: ${city}</p>
-//                      <p>Адрес: ${address}</p>
-//                      <p>Языки: ${languages}</p>`
-//     }
-//
-// })
+window.addEventListener('click', (e) => {
+    if(e.target.type === 'submit'){
+        e.preventDefault();
+        const name = document.querySelector('#name').value;
+        const birthday = document.querySelector('#birthday').value;
+        const m = document.querySelector('#m');
+        const w = document.querySelector('#w');
+        let sex = m;
+        if(m.checked){
+            sex = 'Мужчина'
+        } else {
+            sex = 'Женщина'
+        }
+        const city = document.querySelector('#city').value;
+        const address = document.querySelector('#address').value;
+        const languages = document.querySelectorAll('input[type=checkbox]');
+        const checkedLangs = [];
+        for(const lang of languages){
+            if(lang.checked){
+                checkedLangs.push(lang.value);
+            }
+        }
+
+        document.querySelector('body').innerHTML = `<p>Имя: ${name}</p>
+<p>Дата рождения: ${birthday}</p>
+<p>Пол: ${sex}</p>
+<p>Город: ${city}</p>
+<p>Адрес: ${address}</p>
+<p>Владение языками: ${checkedLangs}</p>`
+    }
+})
